e01f5a3c9de9aeda3ef82f256c356d9e6c5b805f
Florian Lepretre <florian.lprt@gmail.com>
2015-10-20 13:05:42 +0200

    Histoire 14 (Test)

--- test/engineTest.js
+++ test/engineTest.js
@@ -193,3 +193,23 @@ EngineTest.prototype.testBlackStarts = function () {
     var engine = new Engine(true);
     assertTrue(engine.getCurrentPlayer() === 'black');
 };
+
+EngineTest.prototype.testTieGame = function () {
+    var engine = new Engine();
+    var movesList = "a1cbl;d1cbr;b1cbl;e1cbr;c1cbl;f1cbr;a2cbl;d2cbr;b2cbl;e2cbr;c2cbl;f2cbr;" +
+                    "a3cbl;d3cbr;b3cbl;e3cbr;c3cbl;f3cbr;b5ctl;a4ctr;e4ctl;b4ctr;f4ctl;d4ctr;" +
+                    "d5ctl;a5ctr;f5ctl;c4ctr;a6ctl;c5ctr;b6ctl;e5ctr;d6ctl;c6ctr;f6ctl;e6ctr";
+    var move = movesList.split(";");
+
+    for (var key = 0; key < move.length; key++) {
+        engine.play(move[key].substring(0,2));
+        var clockwise     = (move[key].charAt(2) === 'c');
+        var lineOffset    = (move[key].charAt(3) === 't') ? 0 : 1;
+        var columnOffset  = (move[key].charAt(4) === 'l') ? 0 : 1;
+        engine.rotate(lineOffset, columnOffset, clockwise);
+        engine.changeTurn();
+    }
+
+    assertTrue(engine.getBalls() == 36);
+    assertTrue(engine.getWinner() == false);
+};
